142b47e7007d01149f01002d82f1c97eae289258
minsOne <jungminahn.minsone@gmail.com>
2024-05-11 17:02:55 +0900

    modify test code

--- Tests/JSInterfacePluginContainerTests/Mock/MockClosePopupJSPlugin.swift
+++ Tests/JSInterfacePluginContainerTests/Mock/MockClosePopupJSPlugin.swift
@@ -18,16 +18,106 @@ class MockClosePopupJSPlugin: JSInterfacePlugin {
     }
 
     private var closure: ((String, WKWebView) -> Void)?
+
+    private struct Parser {
+       let msg: String
+
+        init?(_ dictonary: [String: Any]) {
+            guard
+                let msg = dictonary["body"] as? String
+            else { return nil }
+
+            self.msg = msg
+        }
+    }
 }
 
-private struct Parser {
-    let msg: String
+class LoadingJSPlugin: JSInterfacePluggable {
+    struct Info {
+        let uuid: String
+        let isShow: Bool
+    }
 
-    init?(_ dictonary: [String: Any]) {
+    let action = "loading"
+
+    func callAsAction(_ message: [String: Any], with webView: WKWebView) {
         guard
-            let msg = dictonary["body"] as? String
-        else { return nil }
+            let result = Parser(message)
+        else { return }
 
-        self.msg = msg
+        closure?(result.info, webView)
+    }
+
+    func set(_ closure: @escaping (Info, WKWebView) -> Void) {
+        self.closure = closure
+    }
+
+    private var closure: ((Info, WKWebView) -> Void)?
+}
+
+private extension LoadingJSPlugin {
+    struct Parser {
+        let info: Info
+
+        init?(_ dictonary: [String: Any]) {
+            guard
+                let uuid = dictonary["uuid"] as? String,
+                let body = dictonary["body"] as? [String: Any],
+                let isShow = body["isShow"] as? Bool
+            else { return nil }
+
+            info = .init(uuid: uuid, isShow: isShow)
+        }
+    }
+}
+
+class PaymentJSPlugin: JSInterfacePluggable {
+    struct Info {
+        let uuid: String
+        let paymentAmount: Int
+        let paymentTransactionId: String
+        let paymentId: String
+        let paymentGoodsName: String
+    }
+
+    let action = "payment"
+
+    func callAsAction(_ message: [String: Any], with webView: WKWebView) {
+        guard
+            let result = Parser(message)
+        else { return }
+
+        closure?(result.info, webView)
+    }
+
+    func set(_ closure: @escaping (Info, WKWebView) -> Void) {
+        self.closure = closure
+    }
+
+    private var closure: ((Info, WKWebView) -> Void)?
+}
+
+private extension PaymentJSPlugin {
+    struct Parser {
+        let info: Info
+
+        init?(_ dictonary: [String: Any]) {
+            guard
+                let uuid = dictonary["uuid"] as? String,
+                let body = dictonary["body"] as? [String: Any],
+                let paymentAmount = body["paymentAmount"] as? Int,
+                let paymentTransactionId = body["paymentTransactionId"] as? String,
+                let paymentId = body["paymentId"] as? String,
+                let paymentGoodsName = body["paymentGoodsName"] as? String
+            else { return nil }
+
+            info = .init(
+                uuid: uuid,
+                paymentAmount: paymentAmount,
+                paymentTransactionId: paymentTransactionId,
+                paymentId: paymentId,
+                paymentGoodsName: paymentGoodsName
+            )
+        }
     }
 }
